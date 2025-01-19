Fix codebook gradients

modules/loss.py
@@ -6,17 +6,17 @@ class ReconstructionLoss(nn.Module):
     def __init__(self) -> None:
         super().__init__()
 
-    def forward(self, x_hat, x) -> Tensor:
+    def forward(self, x_hat: Tensor, x: Tensor) -> Tensor:
         return ((x_hat - x)**2).sum(axis=-1)
 
 
 class CategoricalReconstuctionLoss(nn.Module):
-    def __init__(self, n_cat_feats) -> None:
+    def __init__(self, n_cat_feats: int) -> None:
         super().__init__()
         self.reconstruction_loss = ReconstructionLoss()
         self.n_cat_feats = n_cat_feats
     
-    def forward(self, x_hat, x) -> Tensor:
+    def forward(self, x_hat: Tensor, x: Tensor) -> Tensor:
         reconstr = self.reconstruction_loss(
             x_hat[:, :-self.n_cat_feats],
             x[:, :-self.n_cat_feats]
@@ -31,12 +31,12 @@ class CategoricalReconstuctionLoss(nn.Module):
         return reconstr
 
 
-class RqVaeLoss(nn.Module):
+class QuantizeLoss(nn.Module):
     def __init__(self, commitment_weight: float = 1.0) -> None:
         super().__init__()
         self.commitment_weight = commitment_weight
 
-    def forward(self, query, value) -> Tensor:
-        emb_loss = ((query.detach() - value)**2).sum(axis=[-1, -2])
-        query_loss = ((query - value.detach())**2).sum(axis=[-1, -2])
+    def forward(self, query: Tensor, value: Tensor) -> Tensor:
+        emb_loss = ((query.detach() - value)**2).sum(axis=[-1])
+        query_loss = ((query - value.detach())**2).sum(axis=[-1])
         return emb_loss + self.commitment_weight * query_loss

modules/quantize.py
@@ -5,6 +5,7 @@ from distributions.gumbel import gumbel_softmax_sample
 from einops import rearrange
 from enum import Enum
 from init.kmeans import kmeans_init_
+from modules.loss import QuantizeLoss
 from modules.normalize import L2NormalizationLayer
 from typing import NamedTuple
 from torch import nn
@@ -22,6 +23,7 @@ class QuantizeForwardMode(Enum):
 class QuantizeOutput(NamedTuple):
     embeddings: Tensor
     ids: Tensor
+    loss: Tensor
 
 
 def efficient_rotation_trick_transform(u, q, e):
@@ -46,6 +48,7 @@ class Quantize(nn.Module):
         do_kmeans_init: bool = True,
         codebook_normalize: bool = False,
         sim_vq: bool = False,  # https://arxiv.org/pdf/2411.02038
+        commitment_weight: float = 0.25,
         forward_mode: QuantizeForwardMode = QuantizeForwardMode.GUMBEL_SOFTMAX
     ) -> None:
         super().__init__()
@@ -62,6 +65,7 @@ class Quantize(nn.Module):
             L2NormalizationLayer(dim=-1) if codebook_normalize else nn.Identity()
         )
 
+        self.quantize_loss = QuantizeLoss(commitment_weight)
         self._init_weights()
 
     @property
@@ -106,25 +110,28 @@ class Quantize(nn.Module):
                     -dist, temperature=temperature, device=self.device
                 )
                 emb = weights @ codebook
+                emb_out = emb
             elif self.forward_mode == QuantizeForwardMode.STE:
                 emb = self.get_item_embeddings(ids)
-                emb = x + (emb - x).detach()
+                emb_out = x + (emb - x).detach()
             elif self.forward_mode == QuantizeForwardMode.ROTATION_TRICK:
-                weights = gumbel_softmax_sample(
-                    -dist, temperature=temperature, device=self.device
-                )
-                emb = weights @ codebook
-                emb = efficient_rotation_trick_transform(
+                emb = self.get_item_embeddings(ids)
+                emb_out = efficient_rotation_trick_transform(
                     x / x.norm(dim=-1, keepdim=True),
                     emb / emb.norm(dim=-1, keepdim=True),
                     x
                 )
             else:
                 raise Exception("Unsupported Quantize forward mode.")
+            
+            loss = self.quantize_loss(query=x, value=emb)
+        
         else:
-            emb = self.get_item_embeddings(ids)
+            emb_out = self.get_item_embeddings(ids)
+            loss = self.quantize_loss(query=x, value=emb_out)
 
         return QuantizeOutput(
-            embeddings=emb,
-            ids=ids
+            embeddings=emb_out,
+            ids=ids,
+            loss=loss
         )

modules/rqvae.py
@@ -5,7 +5,7 @@ from einops import rearrange
 from modules.encoder import MLP
 from modules.loss import CategoricalReconstuctionLoss
 from modules.loss import ReconstructionLoss
-from modules.loss import RqVaeLoss
+from modules.loss import QuantizeLoss
 from modules.normalize import l2norm
 from modules.quantize import Quantize
 from modules.quantize import QuantizeForwardMode
@@ -22,6 +22,7 @@ class RqVaeOutput(NamedTuple):
     embeddings: Tensor
     residuals: Tensor
     sem_ids: Tensor
+    quantize_loss: Tensor
 
 
 class RqVaeComputedLosses(NamedTuple):
@@ -86,7 +87,7 @@ class RqVae(nn.Module, PyTorchModelHubMixin):
             CategoricalReconstuctionLoss(n_cat_features) if n_cat_features != 0
             else ReconstructionLoss()
         )
-        self.rqvae_loss = RqVaeLoss(self.commitment_weight)
+        self.rqvae_loss = QuantizeLoss(self.commitment_weight)
     
     @property
     def device(self) -> torch.device:
@@ -109,11 +110,14 @@ class RqVae(nn.Module, PyTorchModelHubMixin):
         gumbel_t: float = 0.001
     ) -> RqVaeOutput:
         res = self.encode(x)
+        
+        quantize_loss = 0
         embs, residuals, sem_ids = [], [], []
 
         for layer in self.layers:
             residuals.append(res)
             quantized = layer(res, temperature=gumbel_t)
+            quantize_loss += quantized.loss
             emb, id = quantized.embeddings, quantized.ids
             res = res - emb
             sem_ids.append(id)
@@ -122,7 +126,8 @@ class RqVae(nn.Module, PyTorchModelHubMixin):
         return RqVaeOutput(
             embeddings=rearrange(embs, "b h d -> h d b"),
             residuals=rearrange(residuals, "b h d -> h d b"),
-            sem_ids=rearrange(sem_ids, "b d -> d b")
+            sem_ids=rearrange(sem_ids, "b d -> d b"),
+            quantize_loss=quantize_loss
         )
 
     @torch.compile
@@ -134,7 +139,7 @@ class RqVae(nn.Module, PyTorchModelHubMixin):
         x_hat = torch.cat([l2norm(x_hat[...,:-self.n_cat_feats]), x_hat[...,-self.n_cat_feats:]], axis=-1)
 
         reconstuction_loss = self.reconstruction_loss(x_hat, x)
-        rqvae_loss = self.rqvae_loss(residuals, embs)
+        rqvae_loss = quantized.quantize_loss
         loss = (reconstuction_loss + rqvae_loss).mean()
 
         with torch.no_grad():