Fix gradient propagation

modules/model.py
@@ -310,10 +310,10 @@ class EncoderDecoderRetrievalModel(nn.Module):
             )
 
         if self.jagged_mode:
-            input_embedding = padded_to_jagged_tensor(input_embedding, lengths=seq_lengths+1)
+            input_embedding = padded_to_jagged_tensor(input_embedding, lengths=(seq_lengths+1), max_len=input_embedding.shape[1])
 
-            seq_lengths_fut = torch.tensor(input_embedding_fut.shape[1], device=input_embedding_fut.device).repeat(B, 1)
-            input_embedding_fut = padded_to_jagged_tensor(input_embedding_fut, lengths=seq_lengths_fut)
+            seq_lengths_fut = torch.tensor(input_embedding_fut.shape[1], device=input_embedding_fut.device).repeat(B)
+            input_embedding_fut = padded_to_jagged_tensor(input_embedding_fut, lengths=seq_lengths_fut, max_len=input_embedding_fut.shape[1])
         
         transformer_context = self.in_proj_context(self.do(self.norm(input_embedding)))
         transformer_input = self.in_proj(input_embedding_fut)
@@ -418,7 +418,8 @@ class EncoderDecoderRetrievalModel(nn.Module):
             log_probas=log_probas.squeeze()
         )
             
-    @torch.compile
+    #@torch.compile
+    # TODO: Fix compile for torch padded -> Jagged
     def forward(self, batch: TokenizedSeqBatch) -> ModelOutput:
         seq_mask = batch.seq_mask
         B, N = seq_mask.shape

modules/rqvae.py
@@ -101,7 +101,7 @@ class RqVae(nn.Module, PyTorchModelHubMixin):
         return next(self.encoder.parameters()).device
     
     def load_pretrained(self, path: str) -> None:
-        state = torch.load(path, map_location=self.device)
+        state = torch.load(path, map_location=self.device, weights_only=False)
         self.load_state_dict(state["model"])
         print(f"---Loaded RQVAE Iter {state['iter']}---")
 

modules/utils.py
@@ -55,12 +55,9 @@ def maybe_repeat_interleave(x, repeats, dim):
 
 
 @torch.compiler.disable
-def padded_to_jagged_tensor(x: Tensor, lengths: Tensor) -> NestedTensor:
-    return torch.nested.nested_tensor(
-        [i[:j.item()] for i, j in zip(x, lengths)],
-        layout=torch.jagged,
-        device=x.device
-    )
+def padded_to_jagged_tensor(x: Tensor, lengths: Tensor, max_len: int) -> NestedTensor:
+    mask = torch.arange(max_len, device=x.device).unsqueeze(0).repeat(x.shape[0], 1) < lengths.unsqueeze(1)
+    return torch.nested.nested_tensor_from_jagged(values=x[mask], lengths=lengths)
 
 
 def jagged_to_flattened_tensor(x: NestedTensor) -> Tensor:

train_rqvae.py
@@ -108,7 +108,7 @@ def train(
     start_iter = 0
     if pretrained_rqvae_path is not None:
         model.load_pretrained(pretrained_rqvae_path)
-        state = torch.load(pretrained_rqvae_path, map_location=device)
+        state = torch.load(pretrained_rqvae_path, map_location=device, weights_only=False)
         optimizer.load_state_dict(state["optimizer"])
         start_iter = state["iter"]+1
 